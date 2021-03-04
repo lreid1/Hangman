2ccafc4b270958b2da896218b9cdaf8baa8f2858
lreid1 <79647991+lreid1@users.noreply.github.com>
2021-03-04 11:24:06 -0300

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -3,10 +3,7 @@
 
 # Hangman Game
 # -----------------------------------
-# Helper code
-# You don't need to understand this helper code,
-# but you will have to know how to use the functions
-# (so be sure to read the docstrings!)
+
 import random
 import string
 #import streamlit as st
